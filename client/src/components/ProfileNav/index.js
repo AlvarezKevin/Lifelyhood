ef09e8e1a1257dc8a1f9b25af7adb323ef25dc25
nalam004 <niharikaalam1234@gmail.com>
2021-04-24 16:15:16 -0400

    updated profile page

--- client/src/components/ProfileNav/index.js
+++ client/src/components/ProfileNav/index.js
@@ -6,18 +6,24 @@ import logout from "../../images/profile/logout.svg";
 import red from "../../images/profile/red.svg";
 import purple from "../../images/profile/purple.svg";
 import blue from "../../images/profile/blue.svg";
+import { useLocation } from 'react-router-dom'
 
 const ProfileNav = () => {
+  let location = useLocation();
+
   return (
     <>
         <Nav>
             <NavLink to="/"><img src={logout} alt="log out" /></NavLink>
-            <NavLink><img src={settings} alt="settings" /></NavLink>
-            <NavLink><img src={calender} alt="calender" /></NavLink>
-            {/*do not show links in home page*/}
-            <NavLink to="/school"><img src={red} alt="nav_school" /></NavLink>
-            <NavLink to="/work"><img src={purple} alt="nav_work" /></NavLink>
-            <NavLink to="/personal"><img src={blue} alt="nav_personal" /></NavLink>
+            <NavLink to="/"><img src={settings} alt="settings" /></NavLink>
+            <NavLink to="/"><img src={calender} alt="calender" /></NavLink>
+            {location.pathname !=="/profile" &&
+              <div>
+                <NavLink to="/school"><img src={red} alt="nav_school" /></NavLink>
+                <NavLink to="/work"><img src={purple} alt="nav_work" /></NavLink>
+                <NavLink to="/personal"><img src={blue} alt="nav_personal" /></NavLink>
+              </div>
+            }
         </Nav>
     </>
   );
