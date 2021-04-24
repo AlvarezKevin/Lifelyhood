9c3cbfbffde84d219cac63107281e5cc729ec014
nalam004 <niharikaalam1234@gmail.com>
2021-04-24 12:21:56 -0400

    added profile models

--- lifelyhood/lifelyhood/models.py
+++ lifelyhood/lifelyhood/models.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profiles/default.jpg', upload_to='profiles')
-    work = models.OneToOneField(WorkSpace, on_delete=models.CASCADE)
-    school = models.OneToOneField(SchoolSpace, on_delete=models.CASCADE)
-    personal = models.OneToOneField(PersonalSpace, on_delete=models.CASCADE)
+    work = models.OneToOneField(Workspace, on_delete=models.CASCADE)
+    school = models.OneToOneField(Schoolspace, on_delete=models.CASCADE)
+    personal = models.OneToOneField(Personalspace, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
+
+class Workspace(models.Model):
+
+class Schoolspace(models.Model):
+    
+class Personalspace(models.Model):
+    
