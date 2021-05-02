c13172e0a56a3a2bd22dc8d79cdf301c15317efd
Kevin Alvarez <kevinalvarez1025@gmail.com>
2021-05-02 17:47:13 -0400

    Backend for notes created

--- backend/settings.py
+++ backend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'notes',
 ]
 
 MIDDLEWARE = [
---
+++ notes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+# Create your models here.
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50,blank=True)
+    text = models.CharField(max_length=10000,blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.username
