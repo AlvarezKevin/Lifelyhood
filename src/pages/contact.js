e57f47b1f7340763fbd1f19c5cc7a917de6633b7
myumbla3 <38135805+myumbla3@users.noreply.github.com>
2021-05-02 09:42:51 -0400

    contact update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import { NavLink as Link } from "react-router-dom";
 import home from "../images/home.svg";
+import "../styles/Contact.css";
 
 export default function Login() {
   return (
@@ -11,24 +12,26 @@ export default function Login() {
           </Link>
         </div>
 
-        <div className="container">
-          
+        <div className="heading">
           <h1><b>Contact Us</b></h1>
-          <h2><b>Get Support</b></h2>
-          <div>
+        </div>
+         
+        <div className="container">
+            <h2><b>Get Support</b></h2>
             <p>
             If you’re having trouble with Lifelyhood, get assistance from other Lifelyhood users or call us at +1 (888) 431 6051.
             </p>
-          </div>
-          <div className="address">
+
+            <hr></hr>
+          
             <h2><b>Lifelyhood Office</b></h2>
-            <h3>New York, United States</h3>
-            <p>Lifelyhood Corporation</p>
-            <p> 151 W 34th St, </p>
-            <p>New York, NY 10001</p>
+            <h4>New York, United States</h4>
+            <p className="address">Lifelyhood Corporation</p>
+            <p className="address"> 151 W 34th St, </p>
+            <p className="address">New York, NY 10001</p>
           </div>
           
-        </div>
+        
         <footer>
           <p>&#169;2021 LIFELYHOOD</p>
         </footer>
