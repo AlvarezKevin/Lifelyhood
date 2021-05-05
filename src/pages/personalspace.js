41f418bdc4fc110d7dfe82fa7f60b07aa987db22
nalam004 <niharikaalam1234@gmail.com>
2021-05-04 22:53:49 -0400

    added styling to personal workspace

--- src/pages/personalspace.js
+++ src/pages/personalspace.js
@@ -7,33 +7,71 @@ import travel from "../images/personal/travel.svg";
 import add from "../images/personal/add.svg";
 import ProfileNav from "../components/ProfileNav";
 import UserInfo from "../components/UserInfo";
+import "../styles/Pages.css"
 import { Link } from "react-router-dom";
 
-const Personalspace = () => {
+const Personalspace = () => {    
   return (
     <div className="profile">
       <ProfileNav />
       <div className="main">
         <UserInfo />
         <div className="pages">
-          <Link>
-            <img src={fitness} alt="Fitness" />
-          </Link>
-          <Link>
-            <img src={health} alt="Health" />
-          </Link>
-          <Link>
-            <img src={grocery} alt="Grocery" />
-          </Link>
-          <Link>
-            <img src={journal} alt="Journal" />
-          </Link>
-          <Link>
-            <img src={travel} alt="Travel" />
-          </Link>
-          <Link>
-            <img src={add} alt="Add Page" />
-          </Link>
+          
+          <div className="page-box">
+            <div className="page-title">
+              <Link className="page-img">
+                <img className="page-ico" src={fitness} alt="Fitness" />
+              </Link>
+              <h5>Fitness</h5>
+            </div>
+          </div>
+
+          <div className="page-box">
+            <div className="page-title">
+              <Link className="page-img">
+                <img className="page-ico" src={health} alt="Health" />
+              </Link>
+              <h5>Health</h5>
+            </div>
+          </div>
+
+          <div className="page-box">
+            <div className="page-title">
+              <Link className="page-img">
+                <img className="page-ico" src={grocery} alt="Grocery" />
+              </Link>
+              <h5>Grocery</h5>
+            </div>
+          </div>
+
+          <div className="page-box">
+            <div className="page-title">
+              <Link className="page-img">
+                <img className="page-ico" src={journal} alt="Journal" />
+              </Link>
+              <h5>Journal</h5>
+            </div>
+          </div>
+
+          <div className="page-box">
+            <div className="page-title">
+              <Link className="page-img">
+                <img className="page-ico" src={travel} alt="Travel" />
+              </Link>
+              <h5>Travel</h5>
+            </div>
+          </div>
+
+          <div className="page-box">
+            <div className="page-title">
+              <Link className="page-img">
+                <img className="page-ico" src={add} alt="Add Page" />
+              </Link>
+              <h5>Add Page</h5>
+            </div>
+          </div>
+    
         </div>
       </div>
     </div>
