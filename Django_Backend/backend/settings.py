ffcd0a4afece25082dc8a30083de4e572f5c287e
Hammad Saeed <42039003+Hammadus@users.noreply.github.com>
2021-05-22 13:13:10 -0400

    Update settings.py

--- Django_Backend/backend/settings.py
+++ Django_Backend/backend/settings.py
@@ -203,5 +203,5 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
-#DATABASES = {}
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600) 
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600) 
