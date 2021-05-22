29683ce97b0c6ee9faf8bb851acc7b66bdab1aea
alvarezkevin <kevinalvarez1025@gmail.com>
2021-05-22 13:47:56 -0400

    Added todo urls:

--- backend/urls.py
+++ backend/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('notes.urls')),
+    path('',include('todo.urls')),
     path('', include('user_calendar.urls')),
     path('', include('apps.accounts.urls')),
     re_path('.*',TemplateView.as_view(template_name='index.html')),
