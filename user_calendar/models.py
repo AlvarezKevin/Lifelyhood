6b3cf195d34bb7227d60d8feb499593be792727c
mneri0 <mneri000@citymail.cuny.edu>
2021-05-24 05:09:23 -0400

    changed model for user_calendar because of datetime.date object error

--- user_calendar/models.py
+++ user_calendar/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Event(models.Model):
     owner = models.ForeignKey('auth.User', related_name='events', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    startDate = models.DateTimeField(default=datetime.now, blank=True)
-    endDate = models.DateTimeField(default=datetime.now, blank=True)
+    startDate = models.DateTimeField(default=timezone.now, blank=True)
+    endDate = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__ (self):
         return f'{ self.title }'
